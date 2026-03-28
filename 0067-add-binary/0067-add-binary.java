459d0bcb1dc30ad141470138bb0f1f1cac55b23a
Mateusz Rokitowski <95221976+m4tiii@users.noreply.github.com>
2026-03-28 23:33:05 +0100

    Time: 1 ms (99.95%), Space: 43.8 MB (27.59%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,18 @@
+class Solution {
+   public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int bitA = (i >= 0) ? a.charAt(i) - '0' : 0;
+            int bitB = (j >= 0) ? b.charAt(j) - '0' : 0;
+            int sum = bitA + bitB + (carry);
+            sb.append(sum % 2);
+            carry = sum / 2;
+            i--;
+            j--;
+        }
+        return sb.reverse().toString();
+    }
+}
